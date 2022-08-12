ce212d3d3381c545c214b98bf03addaaa6bc084e
NAYANE SOUSA MAIA <maia.nayane@gmail.com>
2022-08-12 18:47:39 -0300

    page about us

---
+++ src/components/About/Footer/index.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+function Footer() {
+  return (
+    <footer className="footer__content">
+      <p>© Copyright - Devfire Club</p>
+    </footer>
+  );
+}
+
+export default Footer;
