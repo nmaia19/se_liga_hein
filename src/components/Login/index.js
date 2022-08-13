3f8138699927dd2c61c3286b3b41f05f268cf33e
Renan de Aguiar Modesto <renan.modesto@grupomultiplica.com.br>
2022-08-12 22:06:25 -0300

    Finalizada página de Login / falta funcionalidade form

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -5,10 +5,17 @@ function LoginMain() {
     <div className="login__content">
       <div className="login__account">
         <h1>Login</h1>
-        <label htmlFor="email">E-mail</label>
-        <input type="email" name="email" />
-        <label htmlFor="password">Senha</label>
-        <input type="password" name="password" />
+        <fieldset className="login__fieldset">
+          <label htmlFor="email">E-mail</label>
+          <input type="email" name="email" />
+        </fieldset>
+        <fieldset className="login__fieldset">
+          <label htmlFor="password">Senha</label>
+          <input type="password" name="password" />
+        </fieldset>
+        <a className="login__password" href="/">
+          Esqueceu sua senha?
+        </a>
         <div className="login__button">
           <button>Entrar</button>
         </div>
