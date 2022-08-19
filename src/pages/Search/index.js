5614da0477ab85bbe94e1cd1e5320580731a273a
NAYANE SOUSA MAIA <maia.nayane@gmail.com>
2022-08-19 18:18:24 -0300

    Stylizing search area

--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -1,22 +1,47 @@
 import Card from '../../components/Search/Card'
+import Footer from '../../components/Footer'
 import './styles.css'
 
 function Search() {
+  const filter = ['Cidade', 'Próximos a mim', 'Nome', 'Tipo de Violência']
+
   return (
     <div>
-      <input
-        type="text"
-        placeholder="Digite o seu nome"
-        // onChange={e => setStudentName(e.target.value)}
-      />
-      <select>Filtrar Busca</select>
+      <form onSubmit="" className="search-form">
+        <input
+          className="form__input"
+          placeholder="Digite sua busca"
+          type="text"
+          name="name"
+          // value={form.name}
+          // onChange={handleChange}
+        />
 
-      <button type="button" onClick="">
-        Buscar
-      </button>
+        <select
+          className="form__select"
+          name="category"
+          // onChange={handleChange}
+        >
+          <option value="">Filtrar Busca</option>
+          {filter.map(service => (
+            <option key={service} value={service}>
+              {service}
+            </option>
+          ))}
+        </select>
+
+        <button className="form__btn" type="submit">
+          Buscar
+        </button>
+      </form>
+
       <div className="search__cards">
         <Card />
+        <Card />
+        <Card />
+        <Card />
       </div>
+      <Footer />
     </div>
   )
 }
