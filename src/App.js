40a2df4af3b2ad4df3457c2eb09bd3099353b111
Renan de Aguiar Modesto <renan.modesto@grupomultiplica.com.br>
2022-08-22 16:55:07 -0300

    ajuste login c/ createAccount

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import "./App.css";
 import About from "./pages/About";
 import Login from "./pages/Login";
 import Search from "./pages/Search";
-import CreateAccount from "./pages/CreateAccount";
 import NewOccurrence from "./pages/NewOccurrence";
 import CreateAccount from "./pages/CreateAccount";
 
@@ -17,9 +16,9 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/createaccount" element={<CreateAccount />} />
+        <Route path="/create-account" element={<CreateAccount />} />
         <Route path="/search" element={<Search />} />
-        <Route path="/new-occurrence" element={<NewOccurrence />}/>
+        <Route path="/new-occurrence" element={<NewOccurrence />} />
       </Routes>
     </div>
   );
---
+++ src/pages/CreateAccount/index.js
@@ -0,0 +1,8 @@
+import "./styles.css";
+import FormCreateAccount from "../../components/FormCreateAccount";
+
+function CreateAccount() {
+  return <FormCreateAccount />;
+}
+
+export default CreateAccount;
