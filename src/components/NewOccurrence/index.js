d8a64758cbb5f9db37d56feb20e0e9c59dd93f6f
Renan de Aguiar Modesto <renan.modesto@grupomultiplica.com.br>
2022-08-28 19:15:58 -0300

    ajuste conflito

--- src/components/NewOccurrence/index.js
+++ src/components/NewOccurrence/index.js
@@ -2,7 +2,6 @@ import { useState } from "react";
 import SelectBrazilianStates from "./Form/SelectBrazilianStates";
 import SelectBrazilianCities from "./Form/SelectBrazilianCities";
 import "./styles.css";
-<<<<<<< HEAD
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
@@ -27,14 +26,6 @@ function NewOccurrenceForm() {
   console.log(formData, "teste");
 
   // Select Options
-=======
-import Swal from "sweetalert2";
-import { useNavigate } from "react-router-dom";
-import TermsAndConditions from "../TermsAndConditions";
-
-function NewOccurrenceForm() {
-  const navigate = useNavigate();
->>>>>>> dev
   const options = ["Sim", "Não"];
 
   const violences = [
@@ -50,7 +41,6 @@ function NewOccurrenceForm() {
     "Outro",
   ];
 
-<<<<<<< HEAD
   function submitData(evt) {
     const requestOptions = {
       method: "POST",
@@ -86,99 +76,15 @@ function NewOccurrenceForm() {
     }
     const newValue = isCheckbox ? data : value;
     setFormValues({ ...formValues, [name]: newValue });
-=======
-  const [warning, setWarning] = useState({
-    show: false,
-    message: "",
-  });
-
-  const [formValues, setFormValues] = useState({
-    name: "",
-    victim: "",
-    victimName: "",
-    age: "",
-    violence: "",
-    physicalAggression: "",
-    state: "",
-    city: "",
-    date: "",
-    time: "",
-    local: "",
-    establishment: "",
-    description: "",
-    termsCheck: "",
-  });
-
-  const handleInputChange = (e) => {
-    e.preventDefault();
-    const { name, value } = e.target;
-    setFormValues({ ...formValues, [name]: value });
-  };
-
-  // Remover Warning
-  const warningTime = () => {
-    setTimeout(() => {
-      setWarning({
-        show: false,
-        message: "",
-      });
-    }, 3000);
-    return;
->>>>>>> dev
   };
 
   const handleSubmit = (e) => {
-    // Validar formulário
     e.preventDefault();
-    if (
-      formValues.name === "" ||
-      formValues.victim === "" ||
-      formValues.age === "" ||
-      formValues.violence === "" ||
-      formValues.physicalAggression === "" ||
-      formValues.state === "" ||
-      formValues.city === "" ||
-      formValues.date === "" ||
-      formValues.time === "" ||
-      formValues.local === "" ||
-      formValues.establishment === "" ||
-      formValues.description === "" ||
-      formValues.termsCheck === ""
-    ) {
-      setWarning({
-        show: true,
-        message: "Atenção, todos os campos precisam estar preenchidos.",
-      });
-      warningTime();
-    } else if (formValues.age < 18) {
-      setWarning({
-        show: true,
-        message:
-          "Você precisa ter 18 anos ou mais para cadastrar uma ocorrência.",
-      });
-      warningTime();
-      
-    } else {
-      // Salvar nova ocorrência no localStorage
-      const formData = new FormData(e.target);
-      const occurrence = Object.fromEntries(formData);
-      let occurrences = JSON.parse(localStorage.getItem("occurrences") || "[]");
-      occurrences.push(occurrence);
-      localStorage.setItem("occurrences", JSON.stringify(occurrences));
-
-      // Exibir mensagem 'Ocorrência salva'
-      Swal.fire({
-        position: "center",
-        icon: "success",
-        title: "Sua ocorrência foi registrada",
-        showConfirmButton: false,
-        timer: 1500,
-      });
-      // Redirecionar usuário
-      setTimeout(() => {
-        navigate("/search");
-      }, 2500);
-    }
+    const formData = new FormData(e.target);
+    const occurrence = Object.fromEntries(formData);
+    let occurrences = JSON.parse(localStorage.getItem("occurrences") || "[]");
+    occurrences.push(occurrence);
+    localStorage.setItem("occurrences", JSON.stringify(occurrences));
   };
 
   return (
@@ -196,34 +102,22 @@ function NewOccurrenceForm() {
               type="text"
               name="name"
               id="name"
-<<<<<<< HEAD
               onChange={(e) =>
                 setFormData({ ...formData, name: e.target.value })
               }
               value={formData.name}
-=======
-              onChange={handleInputChange}
-              value={formValues.name || ""}
-              required
->>>>>>> dev
             />
           </fieldset>
 
-          <fieldset className="new-occurrences___form__fieldset">
+          <fieldset classNameName="new-occurrences___form__fieldset">
             <label htmlFor="victim">A vítima é você?</label>
             <select
               id="victim"
-<<<<<<< HEAD
               value={formData.victim}
               name="victim"
               onChange={(e) =>
                 setFormData({ ...formData, victim: e.target.value })
               }
-=======
-              onChange={handleInputChange}
-              value={formValues.victim || ""}
-              required
->>>>>>> dev
             >
               <option>Selecione uma opção</option>
               {options.map((option) => (
@@ -255,36 +149,22 @@ function NewOccurrenceForm() {
               type="number"
               name="age"
               id="age"
-<<<<<<< HEAD
               value={formData.age}
               onChange={(e) =>
                 setFormData({ ...formData, age: e.target.value })
               }
-=======
-              onChange={handleInputChange}
-              value={formValues.age || ""}
-              required
->>>>>>> dev
             />
           </fieldset>
 
           <fieldset className="new-occurrences___form__fieldset">
             <label htmlFor="violenc">Violência sofrida</label>
             <select
-<<<<<<< HEAD
               name="violenc"
               id="violenc"
               value={formData.violenc}
               onChange={(e) =>
                 setFormData({ ...formData, violenc: e.target.value })
               }
-=======
-              name="violence"
-              id="violence"
-              onChange={handleInputChange}
-              value={formValues.violence || ""}
-              required
->>>>>>> dev
             >
               <option>Selecione uma categoria</option>
               {violences.map((violence) => (
@@ -301,15 +181,9 @@ function NewOccurrenceForm() {
               value={formData.physicalAggression}
               name="physicalAggression"
               id="physicalAggression"
-<<<<<<< HEAD
               onChange={(e) =>
                 setFormData({ ...formData, physicalAggression: e.target.value })
               }
-=======
-              onChange={handleInputChange}
-              value={formValues.physicalAggression || ""}
-              required
->>>>>>> dev
             >
               <option value="">Selecione uma opção</option>
               {options.map((option) => (
@@ -325,19 +199,17 @@ function NewOccurrenceForm() {
           <fieldset className="new-occurrences___form__fieldset">
             <label htmlFor="state">Estado</label>
             <SelectBrazilianStates
-              onChange={handleInputChange}
+              onChange={handleLocalChange}
               value={formValues.state || ""}
-              required
             />
           </fieldset>
 
           <fieldset className="new-occurrences___form__fieldset">
             <label htmlFor="city">Cidade</label>
             <SelectBrazilianCities
-              state={formValues.state}
-              onChange={handleInputChange}
+              state={localValues.state}
+              onChange={handleLocalChange}
               value={formValues.city || ""}
-              required
             />
           </fieldset>
 
@@ -347,15 +219,9 @@ function NewOccurrenceForm() {
               type="date"
               value={formData.date}
               id="date"
-<<<<<<< HEAD
               onChange={(e) =>
                 setFormData({ ...formData, date: e.target.value })
               }
-=======
-              onChange={handleInputChange}
-              value={formValues.date || ""}
-              required
->>>>>>> dev
             />
           </fieldset>
 
@@ -365,15 +231,9 @@ function NewOccurrenceForm() {
               type="time"
               value={formData.time}
               id="time"
-<<<<<<< HEAD
               onChange={(e) =>
                 setFormData({ ...formData, time: e.target.value })
               }
-=======
-              onChange={handleInputChange}
-              value={formValues.time || ""}
-              required
->>>>>>> dev
             />
           </fieldset>
         </div>
@@ -385,16 +245,10 @@ function NewOccurrenceForm() {
               name="local"
               id="local"
               placeholder="Digite o endereço"
-<<<<<<< HEAD
               value={formData.local}
               onChange={(e) =>
                 setFormData({ ...formData, local: e.target.value })
               }
-=======
-              onChange={handleInputChange}
-              value={formValues.local || ""}
-              required
->>>>>>> dev
             />
           </fieldset>
 
@@ -404,16 +258,10 @@ function NewOccurrenceForm() {
               type="text"
               name="establishment"
               id="establishment"
-<<<<<<< HEAD
               value={formData.establishment}
               onChange={(e) =>
                 setFormData({ ...formData, establishment: e.target.value })
               }
-=======
-              onChange={handleInputChange}
-              value={formValues.establishment || ""}
-              required
->>>>>>> dev
             />
           </fieldset>
         </div>
@@ -421,21 +269,15 @@ function NewOccurrenceForm() {
           <fieldset className="new-occurrences___form__fieldset">
             <label htmlFor="description">O que aconteceu?</label>
             <textarea
-              placeholder="Por gentileza, descreva da forma mais detalhada possível o que aconteceu"
+              placeholder="Por gentileza, descreva de forma mais detalhada possível o que aconteceu"
               name="description"
               id="description"
               cols="30"
               rows="10"
-<<<<<<< HEAD
               value={formData.description}
               onChange={(e) =>
                 setFormData({ ...formData, description: e.target.value })
               }
-=======
-              onChange={handleInputChange}
-              value={formValues.description || ""}
-              required
->>>>>>> dev
             ></textarea>
           </fieldset>
         </div>
@@ -444,7 +286,30 @@ function NewOccurrenceForm() {
             <label htmlFor="terms" className="terms-label">
               Termos e condições
             </label>
-           <TermsAndConditions className="new-occurrences__terms"/>
+            <textarea name="terms" id="terms" cols="30" rows="10" readOnly>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Repudiandae pariatur aspernatur dolor sapiente laudantium.
+              Nesciunt molestias porro quaerat recusandae labore, enim iusto
+              delectus quod saepe eveniet minima aliquam, eaque est. Lorem ipsum
+              dolor sit amet consectetur adipisicing elit. Modi corrupti nulla
+              omnis ducimus deleniti, architecto expedita facere consectetur
+              quae culpa sed libero similique error ipsum? Vero qui vel sit
+              reiciendis! Lorem ipsum dolor sit amet, consectetur adipisicing
+              elit. Expedita repellat libero nulla ab accusantium id excepturi
+              voluptatum, corporis asperiores debitis enim porro voluptate
+              quibusdam officia eligendi. Consequuntur hic voluptatem ipsum.
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore
+              obcaecati aperiam suscipit eveniet, eos assumenda blanditiis
+              maxime debitis expedita eligendi delectus, magnam quia voluptas
+              impedit accusamus iusto iste fuga temporibus. Lorem ipsum dolor
+              sit amet consectetur adipisicing elit. Qui deleniti ipsum
+              corporis! Impedit vero labore deleniti, eligendi temporibus
+              aliquam distinctio ut officiis ipsam nemo eveniet quasi in quia
+              atque neque? Lorem ipsum dolor, sit amet consectetur adipisicing
+              elit. Saepe asperiores provident ex quis debitis laudantium et
+              recusandae quae, rerum quia porro numquam nihil dignissimos fuga
+              similique architecto possimus. Aliquid, ab!
+            </textarea>
           </fieldset>
         </div>
         <div className="new-occurrences__form__group checkbox-group">
@@ -453,11 +318,10 @@ function NewOccurrenceForm() {
             name="termsCheck"
             id="termsCheck"
             onChange={handleInputChange}
-            required
+            checked={formValues.termsCheck}
           />
           <label htmlFor="termsCheck">Li e aceito os termos e condições.</label>
         </div>
-        {warning.show && <span className="warning">{warning.message}</span>}
         <div className="new-occurrences__form__group">
           <button
             className="newOccurrence__button"
@@ -466,7 +330,6 @@ function NewOccurrenceForm() {
           >
             Registrar ocorrência
           </button>
-          <button className="new-occurrences__btn"type="submit">Registrar ocorrência</button>
         </div>
       </form>
     </div>
