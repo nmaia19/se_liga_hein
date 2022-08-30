c969cef805b456386e1b4c0ba0f3d7a393c18436
Renan de Aguiar Modesto <renan.modesto@grupomultiplica.com.br>
2022-08-30 10:31:20 -0300

    ajuste sem ocorrencias filter

--- src/pages/MyOccurrences/index.js
+++ src/pages/MyOccurrences/index.js
@@ -31,6 +31,7 @@ function MyOccurrences() {
   const handleChange = (event) => {
     setTipodeviolencia(event.target.value);
   };
+  console.log(tipodeviolencia);
 
   useEffect(() => {
     var occorrencesFilter = occorrences.filter((occurrence) => {
@@ -68,57 +69,11 @@ function MyOccurrences() {
       <main className="main__cards">
         <div className="main__cards--div">
           <>
-            {filteredOccorrences.length <= 0
-              ? occorrences.map((item) => {
-                  return (
-                    <div
-                      className="card__occurrence"
-                      style={{ display: "flex" }}
-                    >
-                      <div className="card__occurrence1">
-                        <div className="card__occurrence--headercard">
-                          <p className="name">{item.name}</p>
-                          <p className="violence">{item.violenc}</p>
-                        </div>
-                        <div className="card__info">
-                          <svg
-                            width="9"
-                            height="13"
-                            viewBox="0 0 9 13"
-                            fill="none"
-                            xmlns="http://www.w3.org/2000/svg"
-                          >
-                            <path
-                              d="M4.5 0C2.01214 0 0 2.0345 0 4.55C0 7.9625 4.5 13 4.5 13C4.5 13 9 7.9625 9 4.55C9 2.0345 6.98786 0 4.5 0ZM4.5 6.175C3.61286 6.175 2.89286 5.447 2.89286 4.55C2.89286 3.653 3.61286 2.925 4.5 2.925C5.38714 2.925 6.10714 3.653 6.10714 4.55C6.10714 5.447 5.38714 6.175 4.5 6.175Z"
-                              fill="black"
-                            />
-                          </svg>
-                          <h4 className="card__occurrence--address">
-                            {item.local}
-                          </h4>
-                        </div>
-                        <button className="card__occurrence--button">
-                          Ver mais
-                        </button>
-                      </div>
-                      <div className="card__occurrence__actions">
-                        <div className="card__occurrence--update">
-                          <button>
-                            <img src={iconPencil} alt="" />
-                          </button>
-                        </div>
-                        <div className="card__occurrence--delete">
-                          <button>
-                            <img src={iconDelete} alt="" />
-                          </button>
-                        </div>
-                      </div>
-                    </div>
-                  );
-                })
-              : filteredOccorrences.map((item) => {
-                  return (
-                    <div className="card__occurrence">
+            {tipodeviolencia == 0 ? (
+              occorrences.map((item) => {
+                return (
+                  <div className="card__occurrence" style={{ display: "flex" }}>
+                    <div className="card__occurrence1">
                       <div className="card__occurrence--headercard">
                         <p className="name">{item.name}</p>
                         <p className="violence">{item.violenc}</p>
@@ -144,8 +99,57 @@ function MyOccurrences() {
                         Ver mais
                       </button>
                     </div>
-                  );
-                })}
+                    <div className="card__occurrence__actions">
+                      <div className="card__occurrence--update">
+                        <button>
+                          <img src={iconPencil} alt="" />
+                        </button>
+                      </div>
+                      <div className="card__occurrence--delete">
+                        <button>
+                          <img src={iconDelete} alt="" />
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                );
+              })
+            ) : filteredOccorrences.length > 0 ? (
+              filteredOccorrences.map((item) => {
+                return (
+                  <div className="card__occurrence">
+                    <div className="card__occurrence--headercard">
+                      <p className="name">{item.name}</p>
+                      <p className="violence">{item.violenc}</p>
+                    </div>
+                    <div className="card__info">
+                      <svg
+                        width="9"
+                        height="13"
+                        viewBox="0 0 9 13"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M4.5 0C2.01214 0 0 2.0345 0 4.55C0 7.9625 4.5 13 4.5 13C4.5 13 9 7.9625 9 4.55C9 2.0345 6.98786 0 4.5 0ZM4.5 6.175C3.61286 6.175 2.89286 5.447 2.89286 4.55C2.89286 3.653 3.61286 2.925 4.5 2.925C5.38714 2.925 6.10714 3.653 6.10714 4.55C6.10714 5.447 5.38714 6.175 4.5 6.175Z"
+                          fill="black"
+                        />
+                      </svg>
+                      <h4 className="card__occurrence--address">
+                        {item.local}
+                      </h4>
+                    </div>
+                    <button className="card__occurrence--button">
+                      Ver mais
+                    </button>
+                  </div>
+                );
+              })
+            ) : (
+              <div className="card__occurrence--noregister">
+                <p> Nenhum registro encontrado</p>
+              </div>
+            )}
           </>
         </div>
       </main>
