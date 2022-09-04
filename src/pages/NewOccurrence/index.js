451fb760ac6832763ade5aac7a914046c65c366a
Renan de Aguiar Modesto <renan.modesto@grupomultiplica.com.br>
2022-09-04 14:20:24 -0300

    add Footer

--- src/pages/NewOccurrence/index.js
+++ src/pages/NewOccurrence/index.js
@@ -1,10 +1,12 @@
 import './styles.css'
 import NewOccurrenceForm from '../../components/NewOccurrence/index'
+import Footer from '../../components/Footer'
 
 function NewOccurrence() {
   return (
     <div>
       <NewOccurrenceForm />
+      <Footer/>
     </div>
   )
 }
